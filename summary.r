3f8af1c027c94301a38cc1a1f42145486c5ef4d6
Maud Repellin <47147360+MaudRe@users.noreply.github.com>
2020-03-27 11:23:27 +0100

    Add volcano and MA

--- summary.r
+++ summary.r
@@ -13,5 +13,13 @@ summary = tabItem(tabName = "summary",
     max = 0.1, value = 0.05),
   textOutput("text"),  
   
-  h2("Volcano plot")
+  h1("Clickable Volcano Plots!"),
+  sliderInput('logFcCut', label="log(CPM) cutoff",0,10,2, width="200px"), # Sélection du seuil pour le foldchange
+  sliderInput('padjCut', label="padj cutoff",0,1,0.05, width="200px"),  # Sélection du seuil de la pvalue ajusté
+  plotOutput('volcanoPlot',click='plot_click.Volcano'), #VolcanoPlot
+  tableOutput('clickedPoints.Volcano'), # Tableau correspondant à la zone cliqué du VolcanoPlot
+  plotOutput('maPlot',click='plot_click.MA'), # MA plot
+  tableOutput('clickedPoints.MA') # Tableau correspondant à la zone cliqué du MA plot
 )
+
+         
