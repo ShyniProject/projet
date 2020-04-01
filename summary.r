1f2d52c749710f5859365268ff0e19db4bfa207b
Fjeanneret <38987539+Fjeanneret@users.noreply.github.com>
2020-04-01 21:39:15 +0200

    Data type checking added

--- summary.r
+++ summary.r
@@ -5,16 +5,23 @@ summary = tabItem(tabName = "summary",
   
   ## FILE INPUT
   fileInput("data", label = NULL,
-    buttonLabel = "Browse...",
-    placeholder = "No file selected"),
-  
+            buttonLabel = "Browse...",
+            placeholder = "No file selected",
+            accept = c('text/csv',
+                       'text/comma-separated-values',
+                       'text/tab-separated-values',
+                       'text/plain',
+                       '.csv',
+                       '.tsv')),
+  useShinyalert(), #if data type not supported only
+                  
   h1("Clickable Volcano Plots!"),
-  sliderInput('logFcCut', label="log(CPM) cutoff",0,10,2, width="200px"), # Sélection du seuil pour le foldchange
-  sliderInput('padjCut', label="padj cutoff",0,1,0.05, width="200px"),  # Sélection du seuil de la pvalue ajusté
+  sliderInput('logFcCut', label="log(CPM) cutoff",0,10,2, width="200px"), # SÃ©lection du seuil pour le foldchange
+  sliderInput('padjCut', label="padj cutoff",0,1,0.05, width="200px"),  # SÃ©lection du seuil de la pvalue ajustÃ©
   plotOutput('volcanoPlot',click='plot_click.Volcano'), #VolcanoPlot
-  tableOutput('clickedPoints.Volcano'), # Tableau correspondant à la zone cliqué du VolcanoPlot
+  tableOutput('clickedPoints.Volcano'), # Tableau correspondant Ã  la zone cliquÃ© du VolcanoPlot
   plotOutput('maPlot',click='plot_click.MA'), # MA plot
-  tableOutput('clickedPoints.MA') # Tableau correspondant à la zone cliqué du MA plot
+  tableOutput('clickedPoints.MA') # Tableau correspondant Ã  la zone cliquÃ© du MA plot
 )
 
          
